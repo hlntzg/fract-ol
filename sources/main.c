69d4e9c82fc3f133b75f5ad63af54a0260e8f1a0
Helena Utzig <hutzig@c3r4p1.hive.fi>
2024-08-01 17:43:17 +0300

    fractal_visualization and initial julia and mandelbrot functions

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 12:17:41 by hutzig            #+#    #+#             */
-/*   Updated: 2024/07/31 15:21:30 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/08/01 17:41:48 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,18 +49,63 @@ static void	init_hook(t_fractol *fractol)
 }
 */
 
-static	void	ft_fractal_set(void *param)
+/* x is the z_real and y is the z_img */
+static	int	compute_escape_time(double x, double y, double c_real, double c_img)
+{
+	double	tmp;
+	int	i;
+
+	i = 0;
+	while ((x * x) + (y * y) < 4.0 && i < MAX_ITER)
+	{
+		tmp = x;
+		x = (x * x) - (y * y) + c_real;
+		y = 2 * tmp * y + c_img;
+		i++;
+	}
+	return (i);
+}
+
+static int	mandelbrot(unit32_t pixel_x, unit32_t pixel_y, fractol *fractol)
+{
+	
+	compute_escape_time();
+	if (escape_time == MAX_ITER)
+		return (); //function to color points within the set (black?)
+	else
+		return (); //function to color the pixel (gradient coloring? t = escape_time / MAX_ITER?)
+}
+
+static int	julia(unit32_t pixel_x, unit32_t pixel_y, fractol *fractol)
+{
+
+}
+
+/* this function loops through each pixel on the screen, and use the specific 
+fractal set function to map the pixel to a point in the complex plane and
+determine its color based on the number of iterations before escape */
+static	void	fractal_visualization(void *param)
 {
 	t_fractol	*fractol;
+	unit32_t	x;
+	unit32_t	y;
+	int		pixel_color;
 
 	fractol = (t_fractol *)param;
-	if (ft_strequ(fractol->set, "julia"))
+	x = 0;
+	while (x < WIDTH)
 	{
-		printf("- julia -");
-	}
-	if (ft_strequ(fractol->set, "mandelbrot"))
-	{
-		printf("- mandelbrot -");
+		y = 0;
+		while (y < HEIGHT)
+		{
+			if (ft_strequ(fractol->set, "julia"))
+				pixel_color = julia(x, y, fractol);
+			if (ft_strequ(fractol->set, "mandelbrot"))
+				pixel_color = mandelbrot(x, y, fractol);
+			mlx_put_pixel(fractol->image, x, y, pixel_color);
+			y++;
+		}
+		x++;
 	}
 }
 
@@ -75,7 +120,7 @@ int	main(int argc, char **argv)
 		init_fractol_window(argv[1], &fractol);
 		//init_hook(&fractol);
 		mlx_key_hook(fractol.mlx, fractol_management, &fractol); // check if it needs to be after the loop_hook! 
-		mlx_loop_hook(fractol.mlx, ft_fractal_set, &fractol);
+		mlx_loop_hook(fractol.mlx, fractal_visualization, &fractol);
 		mlx_loop(fractol.mlx);
 		mlx_terminate(fractol.mlx);
 	}
